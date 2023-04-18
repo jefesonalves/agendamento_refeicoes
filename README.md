b94b998fc0ed7f486ecd89168ae6ce0c8a5c2c56
jefesonalves <jefesonbezerra@gmail.com>
2023-04-17 22:46:52 -0300

    atributos com múltiplas seleções

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Sistema para agendamento de refeições do IFCE Campus Iguatu
 
 Bibliotecas utilizadas:
 1. python-decouple para garantir a segurança do Django Secret Key;
+2. django-multiselectfield para funcionar campos com múltiplas seleções;
--- agendamento/admin.py
+++ agendamento/admin.py
@@ -6,7 +6,7 @@ class UtilizadoresAdmin(admin.ModelAdmin):
     list_display = ('matricula', 'nome', 'email', 'localidade')
 
 class ReservaAdmin(admin.ModelAdmin):
-    list_display = ('matricula', 'data_reserva', 'numero_reserva')
+    list_display = ('matricula', 'data_reserva', 'localidade_reserva', 'opcao_refeicao','numero_reserva')
 
 admin.site.register(Utilizadores, UtilizadoresAdmin)
 admin.site.register(Reserva, ReservaAdmin)
